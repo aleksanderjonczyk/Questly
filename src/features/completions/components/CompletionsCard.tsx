5c2983e9f997eba19de88dc03d27debe93ac45b4
aleksanderjonczyk <o.jonczyk2@gmail.com>
2026-03-01 23:12:47 +0100

    add completions title, date and xp to ui in completionsCard

--- src/features/completions/components/CompletionsCard.tsx
+++ src/features/completions/components/CompletionsCard.tsx
@@ -1,8 +1,32 @@
-import type { Completion } from "../../quests/types";
+import type { Completion, Quest } from "../../quests/types";
 
 type CompletionCardProps = {
   completion: Completion;
+  questById: Map<Quest["id"], Quest>;
 };
-export default function CompletionsCard({ completion }: CompletionCardProps) {
-  return <div>{completion.id}</div>;
+export default function CompletionsCard({
+  completion,
+  questById,
+}: CompletionCardProps) {
+  const dateFormat = Intl.DateTimeFormat("pl-PL", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+    timeZone: "CET",
+  });
+  const quest = questById.get(completion.questID);
+  const title = quest ? quest.title : "Unknown quest";
+  return (
+    <div className="completions-card">
+      <div className="completions-card__box">
+        <p className="completions-card__xp">{completion.xp} xp gained</p>
+        <p className="completions-card__title">{title}</p>
+      </div>
+      <p className="completions-card__date">
+        {dateFormat.format(new Date(completion.timestamp))}
+      </p>
+    </div>
+  );
 }
--- src/features/completions/components/CompletionsList.tsx
+++ src/features/completions/components/CompletionsList.tsx
@@ -1,12 +1,20 @@
 import { useEffect, useState } from "react";
-import { fetchCompletions } from "../../quests/api";
-import type { Completion } from "../../quests/types";
+import { fetchCompletions, fetchQuests } from "../../quests/api";
+import type { Completion, Quest } from "../../quests/types";
 import CompletionsCard from "./CompletionsCard";
 
 export default function CompletionsList() {
   const [completions, setCompletions] = useState<Completion[]>([]);
-
+  const [quests, setQuests] = useState<Quest[]>([]);
+  const questById = new Map(quests.map((q) => [q.id, q]));
   //   const todayKey = new Date().toISOString().slice(0, 10);
+  useEffect(function () {
+    async function load() {
+      const data = await fetchQuests();
+      setQuests(data);
+    }
+    load();
+  }, []);
 
   useEffect(function () {
     async function load() {
@@ -20,7 +28,11 @@ export default function CompletionsList() {
     <div className="completions-list">
       {completions
         ? completions.map((completion) => (
-            <CompletionsCard completion={completion} key={completion.id} />
+            <CompletionsCard
+              completion={completion}
+              key={completion.id}
+              questById={questById}
+            />
           ))
         : "Complete quests!"}
     </div>
